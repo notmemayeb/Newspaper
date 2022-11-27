4bbd86f2dc64319a5c9cd2b1b52f5572b676c6dc
Notmemayeb <notmemaybeatwork@gmail.com>
2022-11-27 17:18:40 +0100

    Edit / Delete Article done

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,25 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, DeleteView, UpdateView
+from django.urls import reverse_lazy
 from . import models
 
 
 class ArticleListView(ListView):
     model = models.Article
     template_name = 'article_list.html'
+
+
+class ArticleDetailView(DetailView):
+    model = models.Article
+    template_name = 'article_detail.html'
+
+
+class ArticleUpdateView(UpdateView):
+    model = models.Article
+    fields = ['title', 'body']
+    template_name = 'article_edit.html'
+
+
+class ArticleDeleteView(DeleteView):
+    model = models.Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')

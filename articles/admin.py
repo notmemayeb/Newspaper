1f1e592d86b736ed38a30176e8b7810d62b52b02
Notmemayeb <notmemaybeatwork@gmail.com>
2022-11-27 18:07:29 +0100

    Comments

--- articles/admin.py
+++ articles/admin.py
@@ -3,4 +3,16 @@ from . import models
 
 # Register your models here.
 
-admin.site.register(models.Article)
+
+class CommentInline(admin.StackedInline):
+    model = models.Comment
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+
+admin.site.register(models.Article, ArticleAdmin)
+admin.site.register(models.Comment)
--- articles/models.py
+++ articles/models.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 
 # Create your models here.
 
+
 class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
@@ -18,3 +19,19 @@ class Article(models.Model):
 
     def get_absolute_url(self):
         return reverse('article_detail', args=[str(self.id)])
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments' )
+    comment = models.CharField(max_length=149)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.comment
+
+    def get_absolute_url(self):
+        return reverse('article_list')
+

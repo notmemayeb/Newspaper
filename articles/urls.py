5609396d42badc6c99638059982f89a59183e6b6
Notmemayeb <notmemaybeatwork@gmail.com>
2022-11-27 17:36:15 +0100

    Create article and home.html

--- articles/urls.py
+++ articles/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('<int:pk>/edit/', views.ArticleUpdateView.as_view(), name='article_edit'),
     path('<int:pk>/', views.ArticleDetailView.as_view(), name='article_detail'),
     path('<int:pk>/delete/', views.ArticleDeleteView.as_view(), name='article_delete'),
+    path('new/', views.ArticleCreateView.as_view(), name='article_new'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, DetailView, DeleteView, UpdateView
+from django.views.generic import ListView, DetailView, DeleteView, UpdateView, CreateView
 from django.urls import reverse_lazy
 from . import models
 
@@ -23,3 +23,10 @@ class ArticleDeleteView(DeleteView):
     model = models.Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+
+
+class ArticleCreateView(CreateView):
+    model = models.Article
+    template_name = 'article_new.html'
+    fields = ['title', 'body', 'author']
+

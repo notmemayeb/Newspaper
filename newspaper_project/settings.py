6d5a9bd8df5622540513a282968c4224e355e8ae
notmemayeb <notmemaybeatwork@gmail.com>
2023-02-21 13:42:32 +0100

    Secret key

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 from warnings import warn
 from uuid import uuid1
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
